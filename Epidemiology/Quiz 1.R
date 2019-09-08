2729d2b2f7ad172636da5610309a3d401b011259
Tim Vigers <timothy.vigers@cuanschutz.edu>
2019-09-08 17:27:03 -0600

    epi quiz 1

---
+++ Epidemiology/Quiz 1.R
@@ -0,0 +1,27 @@
+# Q4
+py <- (223 * 4) + (354 * 3) + (112 * 2) + 173
+falls <- 98
+fallsperpy <- falls / py
+round(fallsperpy * 1000)
+
+# Q6
+freq <- 374 / 294567
+round(freq * 100000,1)
+
+#Q8
+new <- 40
+midyear <- 316342
+years <- 2
+rate <- new / midyear * years
+round(rate * 100000,1)
+
+# Q9
+totalcases <- 374 + new
+deathrate <- 3/totalcases
+round(deathrate*1000,1)
+
+# Q11
+cases <- 284
+pop <- 150000
+insdens <- cases / pop
+round(insdens * 100000,1)
