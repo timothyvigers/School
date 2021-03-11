3751823408c11a4425fe54a125f77c7de92aad48
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-03-11 16:30:53 -0700

    still getting weird plots

--- Functional Data Analysis/gam_part2.R
+++ Functional Data Analysis/gam_part2.R
@@ -11,9 +11,9 @@ f2 <- function(x1,x2) sin(pi/2 + x1) + cos(x2^2/4)
 y_x1_x2_f1 <- f1(x1,x2) + rnorm(N)
 y_x1_x2_f2 <- f2(x1,x2) + rnorm(N)
 df_fit <- data.frame(y_x1_x2_f1,y_x1_x2_f2,x1,x2)
-## fit the two models using 100 basis functions
-fit_tprs_x1_x2_f1 <- gam(y_x1_x2_f1 ~ te(x1, x2, k=30, bs="tp"), method="REML", data=df_fit)
-fit_tprs_x1_x2_f2 <- gam(y_x1_x2_f2 ~ te(x1, x2, k=30, bs="tp"), method="REML", data=df_fit)
+## fit the two models 
+fit_te_x1_x2_f1 <- gam(y_x1_x2_f1 ~ te(x1, x2, k=10, bs="cr"), method="REML", data=df_fit)
+fit_te_x1_x2_f2 <- gam(y_x1_x2_f2 ~ te(x1, x2, k=10, bs="cr"), method="REML", data=df_fit)
 # get estimated coefficients
 # grid of new x1, x2 values to predict on
 x1_pred <- seq(min(x1),max(x1),len=nx_pred)
@@ -24,15 +24,15 @@ df_pred <- expand.grid(x1=x1_pred, x2=x2_pred)
 df_pred$f1y = f1(df_pred$x1,df_pred$x2)
 df_pred$f2y = f2(df_pred$x1,df_pred$x2)
 # get actual coefficient estimates
-f1hat_x1_x2 <- predict(fit_tprs_x1_x2_f1, newdata=df_pred, type='terms') 
-f2hat_x1_x2 <- predict(fit_tprs_x1_x2_f2, newdata=df_pred, type='terms')
+f1hat_x1_x2 <- predict(fit_te_x1_x2_f1, newdata=df_pred, type='terms') 
+f2hat_x1_x2 <- predict(fit_te_x1_x2_f2, newdata=df_pred, type='terms')
 # Plot a heatmaps of fhat(x1,x2) - ftrue(x1,x2)
 plt_diff <-
   data.frame(df_pred, 
-             f1hat=df_pred$f1y-f1hat_x1_x2[,"te(x1,x2)"],
-             f2hat=df_pred$f2y-f2hat_x1_x2[,"te(x1,x2)"]) %>%
-  pivot_longer(cols=c("f1hat","f2hat")) %>%
-  ggplot() + theme_bw() +
+             f1diff=df_pred$f1y-f1hat_x1_x2[,"te(x1,x2)"],
+             f2diff=df_pred$f2y-f2hat_x1_x2[,"te(x1,x2)"]) %>%
+  pivot_longer(cols=c("f1diff","f2diff")) %>%
+  ggplot() + theme_bw() +theme_classic(base_size=18) +
   geom_raster(aes(x1,x2,fill=value)) + facet_wrap(~name) +
   scale_fill_gradientn(colours=c("red","white","blue"), limits=c(-2,2))
 
